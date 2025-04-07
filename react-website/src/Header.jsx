f0381b5258d6fcf87b89d30de0d350447b57ae35
Mischa Koevoets <mischakoevoetssch@gmail.com>
2025-04-07 02:12:37 +0200

    Initial Setup

---
+++ react-website/src/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>Portfolio Website</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
